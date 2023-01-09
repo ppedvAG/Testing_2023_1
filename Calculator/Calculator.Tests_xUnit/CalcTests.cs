fe42d447824ef25cee1c59026ba383b3b068e956
André <AndreR@ppedv.de>
2023-01-09 11:24:28 +0100

    Tests xUnit

---
+++ Calculator/Calculator.Tests_xUnit/CalcTests.cs
@@ -0,0 +1,41 @@
+namespace Calculator.Tests_xUnit
+{
+    public class CalcTests
+    {
+        [Fact]
+        public void Sum_2_and_3_results_5()
+        {
+            //Arrange
+            var calc = new Calc();
+
+            //Act
+            var result = calc.Sum(2, 3);
+
+            //Assert
+            Assert.Equal(5, result);
+        }
+
+        [Theory]
+        [InlineData(0, 0, 0)]
+        [InlineData(2, 3, 5)]
+        [InlineData(-2, -3, -5)]
+        [InlineData(-2, 3, 1)]
+        [InlineData(2, -3, -1)]
+        public void Sum(int a, int b, int exp)
+        {
+            var calc = new Calc();
+
+            var result = calc.Sum(a, b);
+
+            Assert.Equal(exp, result);
+        }
+
+        [Fact]
+        public void Sum_MAX_and_1_throws_OverflowException()
+        {
+            var calc = new Calc();
+
+            Assert.Throws<OverflowException>(() => calc.Sum(int.MaxValue, 1));
+        }
+    }
+}
